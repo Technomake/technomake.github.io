58f30dcdc24606be241096a83674302941b0e594
rapidmart <43409651+rapidmart@users.noreply.github.com>
2018-11-27 22:36:53 +0530

    full mod

---
+++ index.php
@@ -0,0 +1,461 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Technomake Electrical Solution</title>
+	<meta name="description" content="Save the power for future. Technomake is a electrical solution company which gives you the perfect energy saving solution." />
+	<meta name="keywords" content="led trader,electrical suppliers, interior designers, electrical contractors, street light ,flood light , hospitality lighting applications, led flood lights, led high bay lights, led garden lights,led high mast light, led tubelight,led industrial lightings,  solar led street lights , supplier from calcutta,kolkata,Jalpaiguri,Kalyani,Gujrat, dubai,Japan, China, India." />
+	<meta name="author" content="Qubits Lab" />
+	<meta name="classification" content="Technomake Electrical Solution Provides Solar Led Street Lights, Led Flood Lights, Led High Bay Lights, Led Garden Lights, Solar Led Street Lights trader,From Kolkata - WestBengal - India. http://www.technomakes.com/">
+	<!-- Favicons-->
+	<link rel="apple-touch-icon" sizes="57x57" href="img/favicons/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="img/favicons/apple-touch-icon-60x60.png">
+	<link rel="icon" type="image/png" href="img/favicons/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="img/favicons/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="img/favicons/manifest.json">
+	
+	<link rel="shortcut icon" href="img/favicons/favicon.ico">
+	<meta name="msapplication-TileColor" content="#00a8ff">
+	<meta name="msapplication-config" content="img/favicons/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
+	<!-- Normalize -->
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<!-- Owl -->
+	<link rel="stylesheet" type="text/css" href="css/owl.css">
+	<!-- Animate.css -->
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.1.0/css/font-awesome.min.css">
+	<!-- Elegant Icons -->
+	<link rel="stylesheet" type="text/css" href="fonts/eleganticons/et-icons.css">
+	<!-- Main style -->
+	<link rel="stylesheet" type="text/css" href="css/cardio.css">
+	
+	
+</head>
+
+<body>
+	<div class="preloader">
+		<img src="img/loader.gif" alt="Preloader image">
+	</div>
+	<nav class="navbar">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"><img src="img/logo.png" data-active-url="img/logo-active.png" alt=""></a>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right main-nav">
+					<li><a href="#intro">Home</a></li>
+					<li><a href="#aboutus">About Us</a></li>
+					<li><a href="#services">Solutions</a></li>
+					<li><a href="#pricing">Pricing</a></li>
+					<li><a href="#footer">Contact</a></li>
+					
+					 <!--<li><a href="#" data-toggle="modal" data-target="#mymodal1" class="btn btn-blue">More Options</a></li> -->
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container-fluid -->
+	</nav>
+	<header id="intro">
+		<div class="container">
+			<div class="table">
+				<div class="header-text">
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<h3 class="light white">Technomake Electrical Solution</h3>
+							<h1 class="white typed">Save today, survive tomorrow....</h1>
+							<span class="typed-cursor" id="aboutus">|</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		
+	</header>
+		<div id="suggestPost" style="z-index:100"><a href="savingscalculator.php" ></a></div>
+	<section>
+		<div class="cut cut-top"></div>
+		<div class="container" >
+			<div class="row intro-tables">
+								<div class="col-md-8" >
+					<div class="intro-table intro-table-third">
+						<h5 class="white heading" >About Us</h5>
+						<div class="owl-testimonials bottom">
+							<div class="item">
+								<h4 class="white heading content">Experience is our Strength.</h4>
+								<h5 class="white heading light author">Technomake Electrical Solution was started with the intention to modernize the electrification with Home automation, Industrial lighting with power saving products , High lumen leds, Solar powered home etc. </h5>
+							</div>
+							<div class="item">
+								<h4 class="white heading content">We believe that we will exceed your expections.</h4>
+								<h5 class="white heading light author">Our team is well-trained and highly motivated to deliver outstanding performance. Careful coordination ensures we’re watching your bottom-line. Whether you need an electrical contractor for a commercial, industrial, or residential project, Technomake Electrical Solution is your best choice.</h5>
+							</div>
+							<div class="item">
+								<h4 class="white heading content">Our design philosophy leads to perfect solution.</h4>
+								<h5 class="white heading light author">&#8226; Detail Engineering based on actual data collection.<br>
+								
+																		&#8226; Two Level Drawing & Data checking.<br>
+																		&#8226; Design Based on Energy Conservation Building Code (ECBC) norms.<br>
+																		&#8226; Converting detail engineering in appropriate drawings & details.
+								</h5>
+							</div>
+							<div class="item">
+								<h4 class="white heading content">Safety is our top priority.</h4>
+								<h5 class="white heading light author">Client safety is our top priority. Our aim is to provide safe & reliable electrical system at optimum costs.</h5>
+							</div>
+							<div class="item">
+								<h4 class="white heading content">We also provide after sell maintenance</h4>
+								<h5 class="white heading light author">We provide 1,2 or 5 years free maintenance after a new product sell or project completion by us. After the completion of free maintenance period, you can also subscribe for paid maintenance.</h5>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="intro-table intro-table-hover">
+						<h5 class="white heading hide-hover">Turn your house into the home of your dreams</h5>
+						<div class="bottom">
+							<h4 class="white heading small-heading no-margin regular">Upgrade Your Home</h4>
+							<h4 class="white heading small-pt">Save 50%</h4>
+							<a href="./rser.html" class="btn btn-white-fill expand">Visit</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+	<section id="services" class="section section-padded">
+		<div class="container">
+			<div class="row text-center title">
+				<h2>Solutions</h2>
+				<h4 class="light muted">Achieve the best results with our wide variety of options!</h4>
+			</div>
+			<div class="row services">
+				<div class="col-md-4">
+					<div class="service">
+						<div class="icon-holder">
+							<img src="img/icons/consult.png" alt="" class="icon">
+						</div>
+						<h4 class="heading">Electrical Consultancy Services</h4>
+						<p class="description">
+						Track and Maintenance.<br>
+						Optimized solution.<br>
+						Code Updates and Corrections.<br>
+						Energy Audits etc.<br>
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="service">
+						<div class="icon-holder">
+							<img src="img/icons/industry.png" alt="" class="icon">
+						</div>
+						<h4 class="heading">Industrial Electrical Works</h4>
+						<p class="description">
+						Sensors, Detectors installation.<br>
+						Electrical Panel Upgrade.<br>
+						Power saving solution.<br>
+						Industrial Lighting etc.<br>
+						
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="service">
+						<div class="icon-holder">
+							<img src="img/icons/home.png" alt="" class="icon">
+						</div>
+						<h4 class="heading">Residential Electrical Works</h4>
+						<p class="description">
+						Savings and Maintenance.<br>
+						Safety and Security.<br>
+						Installation and Upgrades.<br>
+						Diagnosis and Repair etc.<br>
+						
+						
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="service">
+						<div class="icon-holder">
+							<img src="img/icons/ins.png" alt="" class="icon">
+						</div>
+						<h4 class="heading">Industrial Electrical Services</h4>
+						<p class="description">
+						Whole House Surge Protection.<br>
+						Automation implementation.<br>
+						System Modification.<br>
+						Damage Protection.<br>
+						</p>
+					</div>
+				</div>
+				
+				<div class="col-md-4">
+					<div class="service">
+						<div class="icon-holder">
+							<img src="img/icons/install.png" alt="" class="icon">
+						</div>
+						<h4 class="heading">Electrical Installation & Repair</h4>
+						<p class="description">
+						Circuit,Generator Installation.<br>
+						Circuit Breaker Replacements.<br>
+						EV Charger Installation.<br>
+						Wiring Upgrades etc.<br>
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="service">
+						<div class="icon-holder">
+							<img src="img/icons/commercial.png" alt="" class="icon">
+						</div>
+						<h4 class="heading">Commercial Electrical Works</h4>
+						<p class="description">
+						Commercial Energy Audits.<br>
+						Electrical Panel Upgrades.<br>
+						Commercial Specialty Lighting.<br>
+						Commercial Generators and more.<br>
+						</p>
+					</div>
+				</div>
+				
+			</div>
+			<!--<li class="text-center"><a href="#" data-toggle="modal" data-target="#vaservices" class="btn btn-blue">Click here to view all of our services.</a></li>-->
+		</div>
+		
+		<div class="cut cut-bottom"></div>
+	</section>
+	
+	<section id="pricing" class="section">
+		<div class="container">
+			<div class="row title text-center">
+				<h2 class="margin-top white">Pricing</h2>
+				<h4 class="light white">Get a quote today!</h4>
+			</div>
+			<div class="row no-margin">
+				<div class="col-md-7 no-padding col-md-offset-5 pricings text-center">
+					<div class="pricing">
+						<div class="box-main active" data-img="img/pricing1.jpg">
+							<h4 class="white">Residential Services</h4>
+							<h5 class="white regular light">As per <span class="small-font">your requirment</span></h5>
+							<a href="blog/residential-contact-form" target="_blank" class="btn btn-white-fill">Start filling</a>
+							<i class="info-icon icon_question"></i>
+						</div>
+						<div class="box-second active">
+							<ul class="white-list text-left">
+								<li>Fill the form.</li>
+								<li>Our sales team contact you.</li>
+								<li>Know about pricing.</li>
+								<li>If satisfied, place an order.</li>
+								<li>Get benifit</li>
+							</ul>
+						</div>
+					</div>
+					<div class="pricing">
+						<div class="box-main" data-img="img/pricing2.jpg">
+							<h4 class="white">Industrial Services</h4>
+							<h5 class="white regular light">As per<span class="small-font"> your requirment</span></h5>
+							<a href="blog/industrial-contact-form" target="_blank" class="btn btn-white-fill">Start filling</a>
+							<i class="info-icon icon_question"></i>
+						</div>
+						<div class="box-second">
+							<ul class="white-list text-left">
+								<li>Fill the form.</li>
+								<li>Our sales team contact you.</li>
+								<li>Get a quote.</li>
+								<li>If satisfied, place an order.</li>
+								<li>Get benifit.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</section>
+	<section id="cd-google-map">
+			<div id="google-container"></div>
+			<div id="cd-zoom-in"></div>
+			<div id="cd-zoom-out"></div>
+			<address><h6>Technomake Electrical Solution, Naihati, Kolkata- 743135</h6></address> 
+			
+	</section>
+	
+	
+	<!-- mymodel1 -->
+	<div class="modal fade" id="mymodal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog ">
+		
+			<div class="modal-content modal-popup">
+				<a href="#" type="button" class="close-link" data-dismiss="modal"><i class="icon_close_alt2"></i></a>
+				
+				<h3 class="white">Options</h3>
+				<div class="item">
+						<a href="#"  class="btn btn-white-fill">&nbsp &nbsp &nbsp &nbsp <i class="fa fa-calculator"></i> Electrical Calculators &nbsp &nbsp &nbsp &nbsp</a>
+						
+				</div>
+			<div class="row">
+						<div class="col-md-4" >
+						<div class="item " style="text-align:left">
+								<h5 class="white heading light author"><i class="fa fa-wordpress"></i> Blog</h5>
+								<h5 class="white heading light author"><i class="fa fa-flask"></i> Careers </h5>
+								<h5 class="white heading light author"><i class="fa fa-download"></i> Downloads</h5>
+								<h5 class="white heading light author"><i class="fa fa-circle-o"></i> Partners</h5>
+							</div>
+						</div>
+						
+						<div class="col-md-4" >
+						<div class="item" style="text-align:left">
+								
+								<h5 class="white heading light author"><i class="fa fa-question-circle"></i>  Faq</h5>
+								<h5 class="white heading light author"><i class="fa fa-bullhorn"></i> Press Release</h5>
+								<h5 class="white heading light author"><i class="fa fa-users"></i> Teams</h5>
+								<h5 class="white heading light author"><i class="fa fa-smile-o"></i> Life Style</h5>
+							</div>
+						</div>
+						<div class="col-md-4" >
+						<div class="item" style="text-align:left">
+								<h5 class="white heading light author"><i class="fa fa-gamepad"></i> Playground</h5>
+								<h5 class="white heading light author"><i class="fa fa-cogs"></i> Tools</h5>
+								<h5 class="white heading light author"><i class="fa fa-pencil-square-o"></i> Portfolio</h5>
+								<h5 class="white heading light author"><i class="fa fa-sitemap"></i> Sitemap</h5>
+								
+							</div>
+						</div>
+						
+			</div>
+		
+
+				
+			</div>
+		</div>
+	</div>
+	<!-- contact now modal -->
+	<div class="modal fade" id="contactmodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+		
+			<div class="modal-content modal-popup">
+				<a href="#" type="button" class="close-link" data-dismiss="modal"><i class="icon_close_alt2"></i></a>
+				<h3 class="white">Contact Now</h3>
+							<div class="modal-body" style="padding-left:20%">
+									<form method="post" action="process.php">
+										
+										<div style="text-align:left">
+											<h5> 
+											<label class="white" for="name">Name</label><br>
+											<input name="name" type="text" id="name" placeholder="your full name"><br>
+										<br>
+											<label class="white" for="email">Email Address</label><br>
+											<input name="email" type="email" id="email" placeholder="your email address"><br>
+											<br>
+										
+											<label for="comm" class="white">Message</label><br>
+											<textarea name="comments" rows="5" cols="21"id="comm"></textarea><br>
+										
+										<div class="hide">
+											<label for="spam">What is two plus two?</label>
+											<input name="spam" type="text" size="4" id="spam">
+										</div>
+										<br>
+											<input type="submit" class="btn btn-white-fill" name="submit" value="Send">
+											<button type="button" class="btn btn-blue" data-dismiss="modal">Close</button>
+										</div>
+										</h5>
+									</form>
+							</div>
+							
+					
+			</div>
+		</div>
+	</div>
+	<footer id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 text-center-mobile">
+					<h3 class="white">Do business with us</h3>
+					<h5 class="light regular light-white">Feel free to email us to provide some feedback , ask any question or give us suggestions for our development, or to just say Hello!
+					<br><br><i class="fa fa-envelope-o"></i> support@technomakes.com
+					<p><br><i class="fa fa-phone-square"></i> +91 9062885579</p>
+					<p><i class="fa fa-phone-square"></i> +91 9062948923</p>
+<h5>
+					<a href="#" data-target="#contactmodal" href="#" data-toggle="modal" class="btn btn-blue ripple trial-button">Contact Now</a>
+				</div>
+				<div class="col-sm-6 text-center-mobile">
+					<h3 class="white">Opening Hours <span class="open-blink"></span></h3>
+					<div class="row opening-hours">
+						<div class="col-sm-6 text-center-mobile">
+							<h5 class="light-white light">Monday - Friday</h5>
+							<h3 class="regular white">9:00 - 17:00</h3>
+						</div>
+						<div class="col-sm-6 text-center-mobile">
+							<h5 class="light-white light">Saturday</h5>
+							<h3 class="regular white">10:00 - 14:00</h3>
+						</div>
+					</div>
+				
+				</div>
+			</div>
+			<div class="row bottom-footer text-center-mobile">
+				<div class="col-sm-8">
+					<p>&copy; <a href=""> Technomake Electrical Solution</a> , 2017 All Rights Reserved. Developed by <a href="https://www.facebook.com/qubitslab">Qubits Lab</a></p>
+				</div>
+				<div class="col-sm-4 text-right text-center-mobile">
+					<ul class="social-footer">
+						<li><a href="https://www.facebook.com/technomakes"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="https://twitter.com/technomakes"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="https://plus.google.com/+Technomakesol"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+	
+	
+	<!-- Holder for mobile navigation -->
+	<div class="mobile-nav">
+		<ul>
+		</ul>
+		<a href="#" class="close-link"><i class="arrow_up"></i></a>
+	</div>
+	<!-- Scripts -->
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/typewriter.js"></script>
+	<script src="js/jquery.onepagenav.js"></script>
+	<script src="js/main.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCoH5iix9nMi2nrRUX-8eOdsCU-aYfxAiY"></script>
+	<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/567b294844d22c2f50cabf2e/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+</body>
+
+</html>
